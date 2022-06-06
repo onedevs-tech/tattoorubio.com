9405fd11775c1fd786635543d703f1de7babe5f2
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-06 07:55:47 +0200

    friends

---
+++ friends/index.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>tattoo_RUBIO</title>
+      <meta charset="UTF-8" />
+      <link rel="icon" href="../images/fire.png" type="image/x-icon" />
+      <link rel="stylesheet" href="../css/styles.css?v=1.0">
+   </head>
+   <body style="background-color: black;">
+      <?php include '../contact-data.php'; ?>
+      <?php include '../hashtags.php'; ?>
+   </body>
+</html>
