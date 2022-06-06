8fcf607bd8c22ac8ebadb73f8379b80e981efa1a
Jonathan Martinez <jonathan.martinez@habitant.es>
2022-06-06 12:32:09 +0200

    meta responsive

--- bio/index.php
+++ bio/index.php
@@ -3,6 +3,7 @@
       <title>tattoo_RUBIO</title>
       <meta charset="UTF-8" />
       <link rel="icon" href="../images/fire.png" type="image/x-icon" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="../css/styles.css?v=1.0">
    </head>
    <body class="wrapper-bio principal-bg">
