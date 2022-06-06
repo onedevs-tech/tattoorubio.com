ab6031310e35c557ceba169bb674789c88b44959
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-06 17:22:58 +0200

    Truth

--- friends/index.php
+++ friends/index.php
@@ -10,18 +10,12 @@
       <?php include '../contact-data.php'; ?>
       <?php include '../hashtags.php'; ?>
       <div class="friends">
-         <span style="font-size: 0.5em;">♡</span> Cristina <span style="font-size: 0.5em;">♡</span><br />
          Edu<br />
          Kike<br />
+         Miki<br />
          Vero<br />
          Richal<br />
          Juanito<br />
-         Antonio "Palomeke"<br />
-         Ruth "Fumetas"<br />
-         Baltasar<br />
-         Roberto Soldado<br />
-         Khris Middleton<br />
-         Miguel "Tattoo"<br />
       </div>
       <script src="../js/main.js"></script>
    </body>
