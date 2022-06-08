116576b81c6885894a902293612b6b11473c0783
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-08 20:15:19 +0200

    activate

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
    <head>
       <?php include 'head.php'; ?>
    </head>
-   <body style="background-color: black; color: white; font-family: nasalization; font-size: 40pt;">
-      <div style="text-align: center; margin-top: 400px;">Coming soon...</div>
+   <body class="wrapper-home principal-bg">
+      <?php include 'menu-and-contact-data.php'; ?>
+      <?php include 'hashtags.php'; ?>
+      <div class="tattoo-types">
+         Afro<br />
+         Black & Grey<br />
+         Blackwork<br />
+         Custom Style<br />
+         Dotwork<br />
+         Fantasy<br />
+         Neo Japan<br />
+         Neotrad<br />
+         Old School<br />
+         Realistic<br />
+         Tribal<br />
+      </div>
+      <div class="phone">
+         <div><img src="images/whatsapp.svg" align="absmiddle" /></div>
+         <div style="padding-top: 4px;">+34 643 562 490</div>
+      </div>
+      <script src="js/main.js"></script>
    </body>
 </html>
