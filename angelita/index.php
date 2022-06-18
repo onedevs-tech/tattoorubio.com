966212cfb1b24829eab458f5febc7cf215b9d809
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-18 20:25:41 +0200

    angelita

---
+++ angelita/index.php
@@ -0,0 +1,13 @@
+<html>
+   <head>
+      <?php include '../head.php'; ?>
+   </head>
+   <body class="wrapper-angelita">
+      <center>
+         <img src="../images/angelita.png" />
+      </center>
+      <script src="../js/main.js"></script>
+      <?php include '../sounds.php'; ?>
+   </body>
+</html>
+
