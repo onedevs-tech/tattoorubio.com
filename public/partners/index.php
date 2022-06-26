8dd86fe0e49fded3224b806b97b7a620042530e2
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-26 18:15:50 +0200

    tattoo storm granada 80 -> storm granadal

--- public/partners/index.php
+++ public/partners/index.php
@@ -6,9 +6,9 @@
       <?php include '../menu-and-contact-data.php'; ?>
       <?php include '../hashtags.php'; ?>
       <div class="partners">
-         Tatuajes "Mocra Studio" (Spain)<br />
+         Tatuajes "Mocra Studio"<br />
          Lieber Tattoo (Italy)<br />
-         Tattoo "Storm Granada 80" (Spain)<br />
+         Tattoo "Storm Granadal"<br />
       </div>
       <script src="../js/main.js"></script>
       <?php // include '../sounds.php'; ?>
