1c6596027a33067a53f34369f5b10c0f9c6f29bd
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-27 12:11:44 +0200

    quitado Tattoo "Storm Granadal"

--- public/partners/index.php
+++ public/partners/index.php
@@ -8,7 +8,6 @@
       <div class="partners">
          Tatuajes "Mocra Studio"<br />
          Lieber Tattoo (Italy)<br />
-         Tattoo "Storm Granadal"<br />
       </div>
       <script src="../js/main.js"></script>
       <?php // include '../sounds.php'; ?>
