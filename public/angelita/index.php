9229af45d47d33d866910115ec5a06c1bee4bbfc
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-27 12:11:07 +0200

    thoth

--- public/angelita/index.php
+++ public/angelita/index.php
@@ -2,10 +2,37 @@
    <head>
       <?php include '../head.php'; ?>
    </head>
-   <body class="wrapper-angelita">
-      <center>
+   <body>
+      <div class="angelita">
          <img src="../images/angelita.png" />
-      </center>
+      </div>
+      <div class="thoth">
+         <p><b>HISTORIA DE THOTH</b><br />Y DE LOS HIJOS DE LA LUZ</p>
+         <p class="yellow">Porque los Hijos de la Oscuridad son también Hijos de la Luz, pero ellos desconocen las sagradas Leyes Eternas.</p>
+         <p class="yellow">Sólo El Innombrable puede guiar nuestros caminos.</p>
+         <p>Yo, Thoth el Atlante, maestro de Misterios, guardián de los Registros, poderoso rey, mago, viviendo de generación en generación, preparándome para entrar a los Salones de Amenti, dejando la guía de aquéllos que van a pasar, estos registros de la poderosa sabiduría de la Gran Atlántida.</p>
+         <p>En la gran ciudad de Keor, en la isla de Undal, en un tiempo muy lejano, comencé esta encarnación. No como lo hicieron los hombrecitos de la era actual, los poderosos de la Atlántida viven y mueren, pero en lugar de como lo hacían de eones a eones, ellos renovaban su vida en los Salones de Amenti, en donde el río de la vida fluye eternamente hacia delante.</p>
+         <p>Cien veces diez he descendido el camino oscuro que guía a la Luz, y como muchas veces he ascendido de la oscuridad hacia la Luz, mi fuerza y poder se renovaban.</p>
+         <p>Ahora por un tiempo desciendo, y los hombres de Khem (Egipto) ya no me conocerán más.</p>
+         <p>Pero en un tiempo todavía no nacido, surgiré nuevamente, poderoso, requiriendo un informe de aquéllos que quedaron tras de mí.</p>
+         <p>Entonces tengan cuidado, oh hombres de Khem, si vosotros habéis falsamente traicionado mis enseñanzas, puesto que los arrojaré de sus falsos pedestales hacia la oscuridad de las cuevas de las que provienen.</p>
+         <p>No revelen mis secretos a los hombres del norte o a los hombres del sur, para que mi maldición no caiga sobre vosotros.</p>
+         <p>Recuerden y cuiden mis palabras, puesto que volveré nuevamente y requeriré de vosotros eso que cuidan. Así es, incluso desde más allá del tiempo y de la muerte, yo regresaré, recompensando o castigando según el uso que hayan hecho de La Verdad.</p>
+         <p>Grande era mi gente en los antiguos días, grande más allá de la concepción de las pequeñas personas que ahora están a mi alrededor; conociendo la sabiduría de lo antiguo, buscando dentro del corazón del conocimiento infinito que pertenece a la juventud de la Tierra.</p>
+         <p>Sabios éramos nosotros con la sabiduría de los <b>Niños de Luz</b>, quienes habitaban entre nosotros. Fuertes éramos con el poder extraído del Fuego Eterno.</p>
+         <p>Y de todos ésos, el más grande entre los hijos de los hombres, era mi padre, Thotme, guardián del Gran Templo, enlace entre los Niños de Luz que habitaban dentro del Templo y las razas de hombres que moraban en las diez islas.</p>
+         <p>Portavoz, después de los Tres, del Morador de Unal, hablando a los Reyes con la Voz que debe ser escuchada.</p>
+         <p>Crecí ahí de niño a adulto, siendo enseñado por mi padre, hasta que crecí dentro del Fuego de la Sabiduría, hasta que ardió en mí una flama que iluminó mi alma.</p>
+         <p>Nada deseé más que el logro de la Sabiduría Eterna. Hasta que un gran día la orden vino del Morador del Templo, que me presentase frente a él. Pocos habían entre los hijos de los hombres que hubiesen mirado esa poderosa cara y vivido, puesto que no como los hijos de los hombres son los Hijos de la Luz cuando no están encarnados en un cuerpo físico.</p>
+         <p>Elegido fui de los hijos de los hombres, enseñado por el Morador para que sus propósitos fueran cumplidos, propósitos todavía no nacidos en el vientre del tiempo.</p>
+         <p>Largas eras habité en el Templo, aprendiendo siempre y todavía más sabiduría, hasta que yo, también, alcancé la luz emitida del Gran Fuego. Me enseñó Él, el camino a Amenti, el inframundo en el que los grandes Reyes se sientan sobre su trono de poder.</p>
+         <p>Una profunda reverencia hice en homenaje ante los Señores de la Vida y los Señores de la Muerte, recibiendo como mi regalo la Llave de la Vida.</p>
+         <p>Libre fui de los Salones de Amenti, no destinado a estar muerto en el Círculo de la Vida.</p>
+         <p>Lejos a las estrellas viajé hasta que el espacio y el tiempo se convirtieron en nada.</p>
+         <p>Después de haber bebido profundamente de la copa de la Sabiduría, miré en los corazones de los hombres y ahí encontré misterios más grandes, y quedé satisfecho. Puesto que solamente en la Búsqueda de la Verdad puede mi Alma estar tranquila y la flama interior estar apaciguada.</p>
+         <p>A través de las eras viví, viendo a aquéllos a mi alrededor probar de la copa de la muerte y regresar de nuevo a la luz de la vida...</p>
+         <p class="yellow">Continuará</p>
+      </div>
       <script src="../js/main.js"></script>
       <?php // include '../sounds.php'; ?>
    </body>
